de6c8c960ab550c973edf062fa42e5b6bb2ab58e
David Krstevski <krstevskidavid998@gmail.com>
2024-11-29 23:00:32 +0100

    JavaScript

---
+++ homework.JavaScript/hm1/script.js
@@ -0,0 +1,28 @@
+//first task
+let meters = 0.3048
+let feet = 23
+console.log(meters * feet)
+console.log (0.3048 * 23 )
+
+//second task
+
+let length = 83
+let width =  104
+console.log (length * width)
+console.log (83 * 104)
+
+//third task
+let π = 3.14
+let r = 5.2
+console.log ( π * r * r)
+console.log (3.14 * 5.2 * 5.2)
+
+//homework
+let price = 119.95
+let phone = 30
+let persent = 0.05
+let totalPrice = price * phone
+let tax = totalPrice * persent
+let fullPrice = totalPrice + tax
+console.log (fullPrice)
+
