53c031677c0dff2a7f9a700a57c61f5f1c4d1d39
David Krstevski <krstevskidavid998@gmail.com>
2024-12-16 22:24:52 +0100

    Edited

--- homework.JavaScript/hm3/hmPart3/atm.js
+++ homework.JavaScript/hm3/hmPart3/atm.js
@@ -4,10 +4,13 @@ function atm () {
 let balance = 500
 let withdraw = parseInt(prompt('Enter the amount you want to withdraw.'));
 if (Number.isNaN(withdraw)){
-    alert ('Please enter a valid number.');  
+    alert ('Please enter a valid number.'); 
+    return atm(); 
 }
 else if (withdraw > balance){
- alert ("You don't have enough money.")
+ alert ("You don't have enough money.");
+ alert (`Enter bellow ${balance}:`);
+    return atm(); 
 } 
 else {
     balance -= withdraw
